cb67ec2d7ca5e5be1b71bb0b13e7dd568354e591
Yuki Jung <yukiej@gmail.com>
2018-09-24 12:22:05 -0700

    Add Bee class using pseudoclassical style that passes all tests

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,9 @@
 var Bee = function() {
+  Grub.call(this);
+  this.job = 'keep on growing';
+  this.age = 5; 
+  this.color = 'yellow'; 
 };
 
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee; 
